5a13da24b77c40d72ef59af3ee32ef08f6e8a06c
VroomyPatel <59683751+VroomyPatel@users.noreply.github.com>
2020-01-09 13:27:36 +0530

    nothing

---
+++ demo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+string fun();
+
+int main()
+{
+    char name[100];
+    char kill[100];
+
+    //name = fun();
+
+
+    cin.getline(name,sizeof(name));
+    cout<<name;
+
+    //kill = fun();
+    cin.ignore();
+    cin.getline(kill,sizeof(kill));
+    cout<<kill;
+
+
+}
+
+string fun()
+{
+    string hello;
+    cin>>hello;
+
+    return(hello);
+}
+
+// [2,6,1,15]
+// 9
